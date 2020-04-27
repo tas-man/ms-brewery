c71801f9fbf6c62c416d0e7f53b7bf678eb8406f
Totte Sjöman <totte.sjoman@gmail.com>
2020-04-27 07:20:40 +0300

    initial commit

---
+++ src/main/java/tas/springframework/msbrewery/MsBreweryApplication.java
@@ -0,0 +1,13 @@
+package tas.springframework.msbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tas/springframework/msbrewery/MsBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package tas.springframework.msbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
